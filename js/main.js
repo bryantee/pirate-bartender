ca4aae7db011c857d667f904d5be38ac2e9a927b
bryantee <bryantee@gmail.com>
2016-10-10 18:09:28 -0700

    Evaluate response and make drink
    Bartender takes responses from user based on questions and produces a drink to user's liking. Simple working version.

--- js/main.js
+++ js/main.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
   $('.questions').on('click', 'button', function(e) {
     e.preventDefault();
-    console.log("Button clicked to submit preferences.");
+    evaluateResponseAndSetPreferences();
+    serverDrink(theBarTender.createDrink(preference), $('.drink'));
   });
   renderQuestions(questions, $('.questions'));
 });
 
 // Let's setup some variables
 var questions = []; // Questions to ask user in UI
-var preference = {}; // User preference info that from use answers to questions
+var preference = []; // User preference info that from use answers to questions
+
+var theBarTender = new Bartender();
 
 var thePantry = new Pantry( // Container for ingredients, these are all array arguments
   // strong
@@ -50,18 +53,32 @@ questions.push(new Question("bitter", "Is it bitter yer after?"));
 questions.push(new Question("sweet", "What about a little sweetness, las?"));
 questions.push(new Question("fruity", "Oh, you like it fruity doncha?"));
 
+// Render answers in .questions form
 function renderQuestions(questions, parentEl){
 var div = "";
   for (var i = 0; i < questions.length; i++) {
     var questionCount = i + 1;
     var childDiv ="";
-    childDiv += "<div class='form-group'><div class='question'>" + questions[i].question + "</div>";
-    childDiv +=  "<select class='form-control'><option>Yes</option><option>No</option></select>";
+    childDiv += "<div class='form-group'><div class='question'>" + questionCount + ".) " + questions[i].question + "</div>";
+    childDiv +=  "<select data-drink-type='" + questions[i].type + "' class='form-control'><option value='yes'>Yes</option><option value='no'>No</option></select>";
     childDiv += "</div>" // close form-group div
     div += childDiv;
   }
-  div += "<button type='submit' class='btn btn-default'>Make me a drink, lad</button>";
+  div += "<button type='submit' class='btn btn-primary'>Make me a drink, lad</button>";
   parentEl.html(div);
+}
+
+//
+function evaluateResponseAndSetPreferences(){
+  $('select').each(function() {
+    var userPref = $(this).val().toLowerCase();
+      if (userPref === "yes") preference.push($(this).data("drink-type"));
+  });
+}
+
+function serverDrink(drink, parentEl) {
+  parentEl.html("Here's your drink with a " + drink);
+
 }
 
 // Question constructor
@@ -87,5 +104,13 @@ function Ingredient(type, ingredient) {
 
 // Bartender constructor, now with all new functionality!
 function Bartender(){
-  this.createDrink = function() {};
+  this.createDrink = function(preference) {
+    var drink = [];
+    preference.forEach(function(value){
+      var ingredients = thePantry[value];
+      var randomNumber = Math.floor((Math.random() * (ingredients.length - 1)));
+      drink.push(ingredients[randomNumber].ingredient);
+    })
+    return drink.join(" and ");
+  };
 }
